1269683fe65e8e00f978598bad597845cccdbe15
OsamaHayyan <osamahayyan1567@gmail.com>
2025-04-17 23:12:35 +0200

    added custom credential helper methods to user schema

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import * as bcrypt from 'bcryptjs';
 
 @Schema()
 export class User {
@@ -15,3 +16,14 @@ export class User {
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.comparePassword = async function (password: string) {
+    return await bcrypt.compare(password, this.password);
+};

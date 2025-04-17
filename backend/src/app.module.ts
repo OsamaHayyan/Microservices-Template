babf3ba619328290eea55104d8e19ae39e42b017
OsamaHayyan <osamahayyan1567@gmail.com>
2025-04-17 23:14:09 +0200

    implemented basic authentication APIs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import {Module} from '@nestjs/common';
 import {MongooseModule} from '@nestjs/mongoose';
 import {ConfigModule} from '@nestjs/config';
+import {UsersModule} from './users/users.module';
 import {JwtModule} from './jwt/jwt.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
         MongooseModule.forRoot(`${process.env.MONGODB_URI}`),
+        UsersModule,
         JwtModule
     ],
     controllers: [],
---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {User, UserSchema} from "./entities/user.entity";
+import {JwtModule} from "../jwt/jwt.module";
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), JwtModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}

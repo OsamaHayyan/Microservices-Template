0e6b1fbd6a289c33d002edddbf3da2339c6409c3
OsamaHayyan <osamahayyan1567@gmail.com>
2025-05-08 02:53:35 +0300

    refactor: update cookie settings and CORS configuration for improved security and local development

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,13 @@ import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: true,
+        origin: [
+            'http://localhost:8080',
+            'http://localhost:5173',
+            'http://127.0.0.1:8080',
+            'http://127.0.0.1:5173',
+            'http://130.61.130.252:8080',
+        ],
         credentials: true,
     });
     app.use(cookieParser());
